e4d2e4598b743d0ba06e12a9bd2e6c5d87ea3ce5
To2hi <Teodor.jacusdotcom@gmail.com>
2025-08-22 00:19:20 +0200

    Update Fronted.js

--- Page/Fronted.js
+++ Page/Fronted.js
@@ -1,1 +1,30 @@
+import {useExport, useState} From "react";
 
+export defulat function odbiur()
+  Const (Messege, useMessage) = useState{"Nie Otrzymano plików"};
+
+useEffect(( => {
+
+  fetch("api/PAY/creat-order")
+  .then((res) => res.Json())
+  .then(data) =>
+  If(data.string) {
+    SetMessage(data.String);
+  }
+       ))
+          .catch(() => {
+            setMessage("Nie Otrzymano Danych");
+          });
+  },[]);
+
+returne{
+<Div>
+  <H1>Dane Odebrane</H1>
+  <p>{messege}</p>
+</Div>
+    
+}
+
+
+  
+)
