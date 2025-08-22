c0b9748a06aaf1ea833496de81f15ceee00c9997
To2hi <Teodor.jacusdotcom@gmail.com>
2025-08-22 02:38:19 +0200

    Update create-order.js

--- api/payu/create-order.js
+++ api/payu/create-order.js
@@ -1,6 +1,8 @@
 export default async function handler(req, res) {
   if (req.method === "POST") {
     let body = "";
+
+    // odbieranie danych POST
     await new Promise((resolve) => {
       req.on("data", (chunk) => {
         body += chunk.toString();
@@ -8,9 +10,11 @@ export default async function handler(req, res) {
       req.on("end", resolve);
     });
 
+    // parsowanie form-data (x-www-form-urlencoded)
     const params = new URLSearchParams(body);
     const stringValue = params.get("string");
 
+    // wyświetlenie na stronie
     res.status(200).send(`<h1>Dane odebrane: ${stringValue || "brak"}</h1>`);
   } else {
     res.status(200).send("<h1>Nie otrzymano danych</h1>");
