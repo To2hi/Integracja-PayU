0bf2b8133f3a88f3f32079acca08fa86cb50bb82
To2hi <Teodor.jacusdotcom@gmail.com>
2025-08-22 02:20:33 +0200

    Update create-order.js

--- api/payu/create-order.js
+++ api/payu/create-order.js
@@ -1,12 +1,12 @@
 
-export default function hendler(res, req) {
+export default function handler(req, res) {
   if (req.method === "POST")
   {
   const body = req.body;
     res.status(200).json({ string: body.string || null});
   }else 
   {
-    re.status(200).json({ string: null});
+    req.status(200).json({ string: null});
     
   }
   
